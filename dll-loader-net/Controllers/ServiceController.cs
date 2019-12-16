369bbf70aaf57daa6cd2b2e76dcb87c1f99f4267
kamran ghiasvand <kamran.ghiasvand@gmail.com>
2019-12-16 21:34:57 +0330

    some changes

---
+++ dll-loader-net/Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using dll_loader_net.Models;
+using SDK;
+using SDK.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+
+namespace dll_loader_net.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        // GET api/values
+        [HttpPost]
+        public string AddProviderScheduling([FromBody]AddProviderSchedulingEntity entity)
+        {
+
+            Service service = new Service();
+            return service.AddProviderScheduling(entity.Provider,
+                entity.HealthcareFacility,
+               entity.StartDateTime,
+               entity.DurationPerPatient,
+               entity.QueueNumber,
+               entity.Service,
+               entity.Quota,
+               entity.LocalID,
+               entity.BookingStartDate,
+               entity.BookingExpiryDate);
+            //return "OK";
+        }
+
+    }
+}
---
+++ dll-loader-net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dll_loader_net.WebApiApplication" Language="C#" %>
